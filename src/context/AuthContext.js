5fb4e2e02611b11e02d83ded3e8abb001ccf1cc4
EAM26 <emuller2607@gmail.com>
2023-06-13 19:07:55 +0200

    Add register

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,35 +1,46 @@
-import React, { createContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {createContext, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 export const AuthContext = createContext({});
 
-function AuthContextProvider({ children }) {
-  const [isAuth, toggleIsAuth] = useState(false);
-  const navigate = useNavigate();
+function AuthContextProvider({children}) {
+    const [auth, setAuth] = useState({
+        isAuth: false,
+        user: null
+    });
+    const navigate = useNavigate();
 
-  function login() {
-    console.log('Gebruiker is ingelogd!');
-    toggleIsAuth(true);
-    navigate('/profile');
-  }
+    function login() {
+        console.log('Gebruiker is ingelogd!');
+        setAuth({
+            ...auth,
+            isAuth: true,
+            user: null
+        });
+        navigate('/profile');
+    }
 
-  function logout() {
-    console.log('Gebruiker is uitgelogd!');
-    toggleIsAuth(false);
-    navigate('/');
-  }
+    function logout() {
+        console.log('Gebruiker is uitgelogd!');
+        setAuth({
+            ...auth,
+            isAuth: false,
+            user: null
+        });
+        navigate('/');
+    }
 
-  const contextData = {
-    isAuth: isAuth,
-    login: login,
-    logout: logout,
-  };
+    const contextData = {
+        isAuth: auth,
+        login: login,
+        logout: logout,
+    };
 
-  return (
-    <AuthContext.Provider value={contextData}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={contextData}>
+            {children}
+        </AuthContext.Provider>
+    );
 }
 
 export default AuthContextProvider;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {useForm} from "react-hook-form";
+import axios from "axios";
 
 function SignUp() {
+
+    const {register, handleSubmit} = useForm()
+
+    const handleFormSubmit = ((data)=> {
+        void registerUser(data)
+    })
+
+    async function registerUser(data) {
+        try {
+            const response = await axios.post('http://localhost:3000/register', {
+                email: data.email,
+                password: data.password,
+                username: data.username
+            })
+            console.log(response.data)
+        } catch (e) {
+            console.error(e)
+        }
+    }
   return (
     <>
       <h1>Registreren</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque consectetur, dolore eaque eligendi
         harum, numquam, placeat quisquam repellat rerum suscipit ullam vitae. A ab ad assumenda, consequuntur deserunt
         doloremque ea eveniet facere fuga illum in numquam quia reiciendis rem sequi tenetur veniam?</p>
-      <form>
-        <p>*Invoervelden*</p>
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <label htmlFor="email-field">email<input type="email" id="email-field" {...register("email")}/></label>
+          <label htmlFor="password-field">password<input type="password" id="password" {...register("password")}/></label>
+          <label htmlFor="username-field">username<input type="text" id="username-field" {...register("username")}/></label>
+          <button type="submit">Submit</button>
       </form>
       <p>Heb je al een account? Je kunt je <Link to="/signin">hier</Link> inloggen.</p>
     </>
